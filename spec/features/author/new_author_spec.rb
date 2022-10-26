33f701529fbb6d349c9fe8d04e909bff2b378808
Christian Raue <christian.raue@student.hpi.de>
2022-10-26 14:13:14 +0000

    Added new author form

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -4,5 +4,8 @@ describe "New author page", type: :feature do
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
   end
 end
