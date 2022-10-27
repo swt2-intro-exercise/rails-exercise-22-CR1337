c676ce6b666266eaafe309aeb8de7f4e9315f45f
Christian Raue <christian.raue@student.hpi.de>
2022-10-27 12:25:17 +0000

    Added paper validation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be invalid when the user enters an empty title" do
+    paper = Paper.new({"title" => "", "venue" => "test_venue", "year" => "2022"})
+    expect(paper).to_not be_valid
+  end
+
+  it "should be invalid when the user enters an empty venue" do
+    paper = Paper.new({"title" => "test_title", "venue" => "", "year" => "2022"})
+    expect(paper).to_not be_valid
+  end
+
+  it "should be invalid when the user enters an empty year" do
+    paper = Paper.new({"title" => "", "venue" => "test_venue", "year" => ""})
+    expect(paper).to_not be_valid
+  end
+
+  it "should be invalid when the user enters an non integer year" do
+    paper = Paper.new({"title" => "", "venue" => "test_venue", "year" => "no_integer"})
+    expect(paper).to_not be_valid
+  end
 end
